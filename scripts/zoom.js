1652f9b89b2ca0f8d983b80c8b9999b412233be3
Kabirdas Henry <kbrdsk@gmail.com>
2020-04-09 14:24:36 -0700

    introduced 2d array to calculate/draw from

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -1,33 +1,128 @@
+class MandelbrotPoint{
+	constructor(z, cNum){
+		this.z = new ComplexNumber(z.real, z.imag);
+		this.cNum = cNum;
+	}
+}
+
 let canvas = document.getElementById('fractal-canvas');
 let ctx = (canvas.getContext)? canvas.getContext('2d'): null;
+
+let windowArray = [];
+
+let iterations = 100;
 
 let dr = 4;
 let di = 2;
 let r0 = -2;
 let i0 = 1;
 
+for(x = 0; x < +canvas.width; x++){
+	windowArray[x] = [];
+	for(y = 0; y < +canvas.height; y++){
+	let z = convertCoordinates(x, y);
+	windowArray[x][y] = new MandelbrotPoint(z, Infinity);
+	}
+}
+
+
+recomputeArray(windowArray, iterations);
+
+drawFromArray(windowArray);
+
 canvas.addEventListener('click', (e) => zoom(e));
 
-drawFractal();
+//drawFractal();
 
 function zoom(click){
+	iterations += 15;
+	let cornerX = Math.min(Math.max(click.clientX - +canvas.width/4, 0), +canvas.width/2);
+	let cornerY = Math.min(Math.max(click.clientY - +canvas.height/4, 0), +canvas.height/2);
+	let newCoords = convertCoordinates(cornerX, cornerY);
 	dr = dr/2;
 	di = di/2;
-	let newCoords = convertCoordinates(click.clientX, click.clientY);
-	r0 = newCoords[0];
-	i0 = newCoords[1];
-	drawFractal();
+	r0 = newCoords.real;
+	i0 = newCoords.imag;
+	windowArray = scale(windowArray, 2, [cornerX, cornerY]);
+	updateCoords(windowArray);
+	recomputeArray(windowArray, iterations);
+	drawFromArray(windowArray);
+}
+
+function scale(oldArray, factor, corner){
+	let newArray = [];
+	let oldX = corner[0];
+	let width = oldArray.length;
+	let height = oldArray[0].length;
+	for(let x = 0; x < width;){
+		let oldY = corner[1];
+		let oldColumn = oldArray[oldX];
+		let newColumn = [];
+		for(let y = 0; y < height;){
+			for(let j = 0; j < factor; j++){
+				let m = oldColumn[oldY];
+				newColumn[y] = new MandelbrotPoint(m.z, m.cNum);
+				y++;
+				if(y >= height) break;
+			}
+			oldY++;
+		}
+		for(let i = 0; i < factor; i++){
+			newArray[x] = newColumn;
+			x++;
+			if(x >= width) break;
+		}
+		oldX++;
+	}
+	return newArray;	
 }
 
 function checkConvergence(c, iterations){
 	let z = new ComplexNumber(c.real, c.imag);
 	let i;
 	for(i = 0; i < iterations; i++){
-		if(z.abs > 2) break;
+		if(z.abs > 2) return i;
 		z = z.mul(z);
 		z = z.add(c);
 	}
-	return i;
+	return Infinity;
+}
+
+function colorFunction(cVal){
+	return [(cVal * 2) % 255, (cVal * 3) % 255, (cVal * 5) % 255, 255];
+}
+
+function updateCoords(fractalArray){
+	for(let x in fractalArray){
+		for(let y in fractalArray[x]){
+			let oldcNum = fractalArray[x][y].cNum;
+			let newCoords = convertCoordinates(x,y);
+			fractalArray[x][y] = new MandelbrotPoint(newCoords, oldcNum);
+		}
+	}
+}
+
+function recomputeArray(fractalArray, iterations){
+	for(let x in fractalArray){
+		for(let y in fractalArray[x]){
+			//if(fractalArray[x][y].cNum !== Infinity) continue;
+			let z = fractalArray[x][y].z;
+			fractalArray[x][y].cNum = checkConvergence(z, iterations);
+		}
+	}
+}
+
+function drawFromArray(array){
+	let width = array.length;
+	let height = array[0].length;
+	let imageData = ctx.createImageData(width, height);
+	for(let x = 0; x < width; x++){
+		for(let y = 0; y < height; y++){
+			let cVal = convertTocVal(array[x][y].cNum, iterations);
+			drawPixel(imageData, [x, y], colorFunction(cVal));
+		}
+	}
+	ctx.putImageData(imageData, 0, 0);
 }
 
 function drawFractal (){
@@ -36,8 +131,8 @@ function drawFractal (){
 	let imageData = ctx.createImageData(width, height);
 	for(let x = 0; x < width; x++){
 		for(let y = 0; y < height; y++){
-			let cVal = colorPixel(x,y);
-			drawPixel(imageData, [x, y], [(cVal * 2) % 255, (cVal * 3) % 255, (cVal * 5) % 255, 255]);
+			let cVal = getcVal(x,y);
+			drawPixel(imageData, [x, y], colorFunction(cVal));
 		}
 	}
 	ctx.putImageData(imageData, 0, 0);
@@ -51,14 +146,21 @@ function drawPixel (imageData, [pixelX, pixelY], rgba){
 
 function convertCoordinates(x, y){
 	let relX = x/(+canvas.width);
-	let relY = -y/(+canvas.height);
+	let relY = y/(+canvas.height);
 
-	return [r0 + relX * dr, i0 + relY * di];
+	return new ComplexNumber(r0 + relX * dr, i0 - relY * di);
 }
 
-function colorPixel(...coords){
-	let z = new ComplexNumber(...convertCoordinates(...coords));
-	let iterations = 100;
-	let alpha = checkConvergence(z, iterations)/iterations * 255;
-	return alpha; 
+function convertTocVal(cNum, iterations){
+	let cN = Math.min(iterations, cNum);
+	let cVal = cN/iterations * 255;
+	return cVal; 
 }
+
+function getcVal(x, y, iterations = 100){
+	let z = convertCoordinates(x, y);
+	let cNum = Math.min(iterations, checkConvergence(z, iterations));
+	let cVal = cNum/iterations * 255;
+	return cVal; 
+}
+
